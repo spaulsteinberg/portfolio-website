88afb495f3d1d288bfd8e027965f89be567920ab
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-01-06 21:14:55 -0600

    changed picture

--- src/components/Body/ProjectInfo/RestAdminProject.js
+++ src/components/Body/ProjectInfo/RestAdminProject.js
@@ -8,7 +8,7 @@ const RestAdminProject = () => {
     const title = "rAdmin (Restaurant Tool)"
     const subTitleText = "A full-service app for small restaurants";
     const stack = ['React', 'React TypeScript', 'Redux','Firebase', 'Firebase Auth', 'Stripe API', 'Sass', 'Java', 'Spring Boot', 'Docker'];
-    const backgroundTitleImage = `${process.env.PUBLIC_URL}/public-project-images/rest-admin/rest_order_art.png`;
+    const backgroundTitleImage = `${process.env.PUBLIC_URL}/public-project-images/rest-admin/order-2_art.png`;
     const titleDisplayColor = "white";
     const description = `
         <p>
